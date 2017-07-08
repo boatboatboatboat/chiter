7957d193bad85c9f510926dddc7f2499763517bd
boat <boat@cock.li>
2017-07-08 23:55:13 +0200

    added endianness stuff.

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,100 @@
 #[macro_export]
+/// Creates a function from an address where:
+/// $addy = function address
+/// $res = return type
+/// $(arg)* = argument types
+/// Example:
+/// ```rust
+/// // in C: int foo() { return 1; }
+/// make_func!(address_of_foo, isize, )
+/// // in C: int bar(int baz, int foobar) { return baz + foobar; }
+/// make_func!(address_of_bar, isize, isize, isize)
+/// ```
 macro_rules! make_func {
     ($addy:expr, $res:ty, $($arg:ty),*) => {
         std::mem::transmute::<*const usize, fn($($arg,)*) -> $res>($addy as *const usize)
     }
 }
 
-pub fn read_bytes(address: usize, length: usize) -> Vec<u8> {
-    let mut res = Vec::<u8>::new(); //T-T-TURBOFISH
-    let mut i = 1;
-    while i <= length {
-        unsafe{res.push(*((address + (length - i)) as *const u8))};
-        i += 1;
-    };
-    res
+// an enum for endianess
+pub enum Endian { Big, Little }
+
+pub struct memory {
+    end: Endian
 }
 
-pub fn read_usize(address: usize) -> usize {
-    let res = unsafe { *((address as usize) as *mut usize) };
-    res
-}
-
-pub fn write_bytes(address: usize, bytes: &[u8]) {
-    let mut i = 1;
-    let length = bytes.len();
-    for byte in bytes.into_iter() {
-        unsafe { *((address + (length - i) as usize) as *mut u8) = *byte };
-        i += 1;
+impl Memory {
+    /// Returns a struct thing with all the functions, prepared w/ endianess.
+    pub fn new(endian: Endian) -> Memory {
+        Memory {end: endian}
     }
-}
-
-pub fn write_usize(address: usize, writeme: usize) {
-    unsafe { *((address as usize) as *mut usize) = writeme };
-}
-
-
-pub fn search(pattern: &[u8], from: usize, to: usize, wildcard: u8) -> usize {
-    if from >= to {
-        panic!("the from address is higher than the to address");
+    // Reads "length" amount of bytes starting at address.
+    pub fn read_bytes(&self, address: usize, length: usize) -> Vec<u8> {
+        let mut result = Vec::<u8>::new(); //TURBO
+        let mut i; // we know we're going to index it rn.
+        match self.end {
+            Endian::Big => {
+                i = 0;
+                while i <= length {
+                    unsafe { result.push(*((address + i) as *const u8)) };
+                    i += 1;
+                }
+            },
+            Endian::Little => {
+                i = 1;
+                while i <= length {
+                    unsafe { result.push(*((address + length - i) as *const u8)) };
+                    i += 1;
+                }
+            }
+        }
+        result
     }
-    for position in from..to {
-        let ressy = read_bytes(position, pattern.len());
-        let mut p = 0;
-        for res_byte in &ressy {
-            if pattern[p] == wildcard {
-                p += 1;
-                continue // Wildcard, just skip.
+    // Writes bytes, starting at address.
+    pub fn write_bytes(&self, address: usize, bytes: &[u8]) {
+        let length = bytes.len();
+        let mut i;
+        match self.end {
+            Endian::Big => {
+                i = 1; // this should fix it LOL
+                for byte in bytes.into_iter() {
+                    println!("GenAddr: {:x}", unsafe { *((address + i) as *mut u8) });
+                    println!("GenByte: {:x}", *byte);
+                    unsafe { *((address + i) as *mut u8) = *byte };
+                    i += 1;
+                }
+            },
+            Endian::Little => {
+                i = 1;
+                for byte in bytes.into_iter() {
+                    unsafe { *((address + length - i) as *mut u8) = *byte };
+                    i += 1;
+                }
             }
-            if res_byte != &pattern[p] {
-                break // Pattern does not match.
-            } else if p == pattern.len() - 1 {
-                return position // We found it!
-            }
-            p += 1;
         }
     }
-    0 // Return 0 if we don't find anything :c
+    /// Searches for a matching pattern between the addresses "from" and "to".
+    pub fn search(&self, pattern: &[u8], from: usize, to: usize, wildcard: u8) -> usize {
+        if from >= to {
+            panic!("the from address is higher than the to address");
+        }
+        let length = pattern.len() - 1;
+        for position in from..to {
+            let bytes = self.read_bytes(position, length);
+            let mut p = 0;
+            for byte in &bytes {
+                if pattern[p] == wildcard {
+                    p += 1;
+                    continue
+                }
+                if byte != &pattern[p] {
+                    break
+                } else if p == length {
+                    return position - 1
+                }
+                p += 1;
+            }
+        }
+        0
+    }
 }
